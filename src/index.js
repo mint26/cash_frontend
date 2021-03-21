34b698802a9f1f2e9efac77e58cfd03a9d37fa6e
Hui Min Tan <huimin.tan26@gmail.com>
2021-03-21 13:24:28 +0800

    remove cssBaseline component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
 import theme from "./theme.js";
@@ -21,7 +20,6 @@ const store = createStore(
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <App />
     </ThemeProvider>
   </Provider>,
